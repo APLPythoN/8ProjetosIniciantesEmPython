f19440e3e04478d2df977251c4afb59a7b603404
André de Paula <111670251+APLPythoN@users.noreply.github.com>
2022-09-18 04:11:48 +0000

    Foi criado um jogo de dado, onde por através das importações (Random) e (PySimpleGUI), geramos um mini Layout com uma janela onde apresenta ao usuário dois botões de Sim e Não os quais gerariam um valor aleatório entre 0 e 6 caso positivo e um agradecimento pela participação caso negativa as respostas.

--- main.py
+++ main.py
@@ -1,71 +1,37 @@
-#EX0
-# from random import randint
-# computador = randint(0, 10)
-# acertou = False
-# print('Jogo da advinhação...')
-# palpites = 0
+# Simulador de dado
+# Simular o uso de um dado, gerando um valor de 1 até 6.
+import random
+import PySimpleGUI as sg
 
+class SimuladorDeDado:
+    def __init__(self):
+        self.valor_minimo = 1
+        self.valor_maximo = 6
+        #layout
+        self.layout = [
+          [sg.Text('Jogar o dado? ')],
+          [sg.Button('Sim'), sg.Button('Não')]
+        ]
+      
+    def Iniciar(self):
+        #criar uma janela
+        self.janela = sg.Window('Simulador de Dado ', layout = self.layout)
+        #ler os valores da tela
+        self.eventos, self.valores = self.janela.Read()      
+        #fazer alguma coisa com esses valores.
+        try:
+          if self.eventos in 'Simsim':
+            self.GerarValorDoDado()
+          elif self.eventos in 'Naonaonão':
+            print('Agradecemos sua participação! ')
+          else:  
+            print('Favor digitar [SIM/NAO].')
+        except:
+          print('Ocorreu um erro ao receber sua resposta.')
 
-# while not acertou:
-#   jogador = int(input('Digite um número entre 0 e 10 para ver se acertou: '))
-#   if jogador == computador:
-#     palpites += 1
-#     acertou = True
-#     print('Acertou o palpite! ')
-#   else:
-#     if jogador > computador:
-#       palpites += 1
-#       print('Menos, tente novamente...')
-#     elif jogador < computador:
-#       palpites += 1
-#       print('Mais, tente novamente...')
-# print(f'Fim de programa com {palpites} tentativas. ')
+        
+    def GerarValorDoDado(self):
+        print(random.randint(self.valor_minimo, self.valor_maximo))
 
-
-#EX1
-
-#salario = float(input('Salario do funcionário: '))
-# dias = int(input('Quantos dias por mês trabalha? '))
-# horas = int(input('Quantas horas por dia trabalha ? '))
-# ganho = (salario / dias) / horas
-# print(ganho)     
-
-
-#EX2
-
-# nome = str(input('QUal é o seu nome? '))
-# print(f'Seja bem vindo {nome} ! ')
-
-# n1 = int(input('Primeiro valor: '))
-# n2 = int(input('Segundo valor: '))
-# if n1 > n2:
-#   print('O maior é o primeiro. ')
-# else:
-#   print('O maior é o segundo. ')
-
-
-# EX3
-
-#n = int(input('Digite um número para calcular seu fatoria: '))
-# c = n
-# f = 1
-# print(f'Calculando {n}! ')
-# while c > 0:
-#   print(f'{c}', end = '')
-#   if c > 1:
-#     print(' x ', end= '')
-#   else:
-#     print(' = ', end= '')
-#   f *= c
-#   c -= 1
-# print(f'O fatorial é {f}. ')
-
-# EX4
-
-# idades = [15, 46, 75, 34, 23]
-# total = 0
-# for idade in idades:
-#   print(idade, idade)
-#   total += idade
-# print(total)
-
+simulador = SimuladorDeDado()
+simulador.Iniciar()
