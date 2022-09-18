2c815e5e8a2a07f0b81584f15dae530cadd1bb49
André de Paula <111670251+APLPythoN@users.noreply.github.com>
2022-09-18 06:17:09 +0000

    Foi criado um algorítmo onde o computador gera um número aleatório entre 0 e 100 e pede para que o usuário chute um número para tentar acertar e caso o usuário erre, o programa saberá informar se o chute foi maior ou menor do que o número que foi gerado pelo computador e apartir daí dirá para ele chutar um número maior ou menor do que a vez anterior.

---
+++ 8Projetos-Python_Iniciantes_Com_Jonathan-DEV-APRENDER/jogo_de_dado.py
@@ -0,0 +1,37 @@
+# Simulador de dado
+# Simular o uso de um dado, gerando um valor de 1 até 6.
+import random
+import PySimpleGUI as sg
+
+class SimuladorDeDado:
+    def __init__(self):
+        self.valor_minimo = 1
+        self.valor_maximo = 6
+        #layout
+        self.layout = [
+          [sg.Text('Jogar o dado? ')],
+          [sg.Button('Sim'), sg.Button('Nao')]
+        ]
+      
+    def Iniciar(self):
+        #criar uma janela
+        self.janela = sg.Window('Simulador de Dado ', layout = self.layout)
+        #ler os valores da tela
+        self.eventos, self.valores = self.janela.Read()      
+        #fazer alguma coisa com esses valores.
+        try:
+          if self.eventos in 'Simsim':
+            self.GerarValorDoDado()
+          elif self.eventos == 'Nao' or self.eventos == 'n':
+            print('Agradecemos sua participação! ')
+          else:  
+            print('Favor digitar [SIM/NAO].')
+        except:
+          print('Ocorreu um erro ao receber sua resposta.')
+
+        
+    def GerarValorDoDado(self):
+        print(random.randint(self.valor_minimo, self.valor_maximo))
+
+simulador = SimuladorDeDado()
+simulador.Iniciar()
